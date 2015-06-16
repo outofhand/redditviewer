05d006791770d6c808659bc164c23ed431ad7037
outofhand <rnoroian@gmail.com>
2015-06-16 14:01:23 -0400

    Changed web.js

--- web.js
+++ web.js
@@ -1,7 +1,13 @@
-var gzippo = require('gzippo');
 var express = require('express');
 var app = express();
- 
-app.use(express.logger('dev'));
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 5000);
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
