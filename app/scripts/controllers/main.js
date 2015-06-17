d75d97333a62f4c461ccce2d72a603c3f798c33a
outofhand <rnoroian@gmail.com>
2015-06-17 13:25:22 -0400

    Cleaned up code

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,18 @@
  * Controller of the redditApp
  */
 angular.module('redditApp')
-  .controller('MainCtrl', function ($scope, $http, $sce, JobManager, Reddit) {
+  .controller('MainCtrl', function ($scope, $http, $sce, Reddit) {
+
+	$scope.reddits = [
+		{label: "awww"},
+		{label: "funny"},
+		{label: "pics"},
+		{label: "Creatures_of_earth"},
+		{label: "GrilledCheese"},
+		{label: "nonononoYES"},
+		{label: "UNBGBBIIVCHIDCTIICBG"},
+		{label: "wtf"}
+	];
 
   	$scope.reddit = new Reddit();  	
 
@@ -23,281 +34,53 @@ angular.module('redditApp')
 	    return $sce.trustAsHtml(decoded);
 	};	
 
+	// get the reddit posts for the subreddit selected
 	$scope.getNextPage = function (subreddit) {
 		$scope.reddit = new Reddit();
-		//$scope.subreddit = subreddit;
 		$scope.reddit.nextPage(subreddit);		
 	}	
 
-	$scope.reddits = [{label: "awww"},{label: "funny"},{label: "pics"},{label: "Creatures_of_earth"},{label: "GrilledCheese"},{label: "nonononoYES"}];
-
+	// get subreddit selected from slider menu
 	$scope.setSelected = function (reddit) {
 		$scope.subreddit = reddit;
 		$scope.checked = !$scope.checked
 	}
 
+	// toggle for subreddit list slider
     $scope.checked = false; // This will be binded using the ps-open attribute
     $scope.toggle = function(){
         $scope.checked = !$scope.checked
     }	
 
-    $scope.toggleComments = function(){
-        $scope.checked2 = !$scope.checked2
-    }
-
+    // toggle for comments slider
     $scope.checked2 = false; // This will be binded using the ps-open attribute
     $scope.toggle2 = function(postId){
+    	//$scope.reddit.getComments($scope.subreddit, postId); 
         $scope.checked2 = !$scope.checked2
         if ($scope.checked2) {
-        	//$scope.getComments(postId);
-console.log('postId = ' + postId + ' subreddit = ' + $scope.subreddit);       	
-        	//$scope.getComments2($scope.subreddit, postId);
-        	$scope.getComments2(postId);        	
+        	$scope.reddit.getComments($scope.subreddit, postId); 
+        	//$scope.checked2 = !$scope.checked2;     	
         }
-    }   
+    }  
 
-	$scope.getNext = function () {
-		$scope.definitions = {};
-		$scope.posts = [];
-		$scope.spinner = true;
-		console.log('next page = ' + $scope.nextPage);
-	    JobManager.getNext($scope.subreddit, $scope.nextPage).then(function(data) {
-	        $scope.spinner = false;
-
-	        if (data.length === 0) {
-	        //if (Object.keys(data.data.children).length === 0) {
-	        	//$scope.definitions = {'definitions': [{'text':'No results found', 'attribution': ''}]};
-	        	$scope.error = true;
-	        	$scope.message = 'No results found for subreddit ' + $scope.subreddit;	
-	        	console.log('error');
-	        } else {
-
-	          var items = data.data.children;
-	          for (var i = 0; i < items.length; i++) {
-
-	            if(!items[i].data.is_self)
-	            {
-
-	              var imgurPattern = /imgur\.com/;
-
-	              if (imgurPattern.test(items[i].data.url)) {
-	                var gifvPattern = /.gifv$/;
-	                
-	                if(!gifvPattern.test(items[i].data.url))
-	                {
-	                    items[i].data.url = items[i].data.url+'.png';
-
-
-	                    var thumb = '';
-	                    if (items[i].data.media != null) {
-	                    	thumb = items[i].data.media.oembed.thumbnail_url;
-	                    } else {
-	                    	thumb = items[i].data.url;
-	                    }
-
-	                    var newItem = {
-	                      id: items[i].data.id,	
-	                      url: items[i].data.url,
-	                      title: items[i].data.title,
-	                      permalink: 'http://reddit.com'+items[i].data.permalink,
-	                      subreddit: items[i].data.subreddit,
-	                      ups: items[i].data.ups,
-	                      author: items[i].data.author,
-	                      domain: items[i].data.domain,
-	                      thumbnail: thumb,
-	                      commentCount: items[i].data.num_comments
-	                    }
-
-	                    $scope.posts.push(newItem);
-
-
-	                }
-	              }
-
-	            }
-
-	            $scope.nextPage = data.data.after;
-	          }
-          	} 
-	    });
-
-	}
-
-	$scope.findWord = function(subreddit) {
-		$scope.definitions = {};
-		$scope.posts = [];
-		$scope.spinner = true;
-	    JobManager.getAll(subreddit).then(function(data) {
-	        $scope.spinner = false;
-
-	        if (data.length === 0) {
-	        //if (Object.keys(data.data.children).length === 0) {
-	        	//$scope.definitions = {'definitions': [{'text':'No results found', 'attribution': ''}]};
-	        	$scope.error = true;
-	        	$scope.message = 'No results found for subreddit ' + subreddit;	
-	        	console.log('error');
-	        } else {
-
-	          var items = data.data.children;
-	          for (var i = 0; i < items.length; i++) {
-
-	            if(!items[i].data.is_self)
-	            {
-
-	              var imgurPattern = /imgur\.com/;
-
-	              if (imgurPattern.test(items[i].data.url)) {
-	                var gifvPattern = /.gifv$/;
-	                
-	                if(!gifvPattern.test(items[i].data.url))
-	                {
-	                    items[i].data.url = items[i].data.url+'.png';
-
-
-	                    var thumb = '';
-	                    if (items[i].data.media != null) {
-	                    	thumb = items[i].data.media.oembed.thumbnail_url;
-	                    } else {
-	                    	thumb = items[i].data.url;
-	                    }
-
-	                    var newItem = {
-                    	  id: items[i].data.id,
-	                      url: items[i].data.url,
-	                      title: items[i].data.title,
-	                      permalink: 'http://reddit.com'+items[i].data.permalink,
-	                      subreddit: items[i].data.subreddit,
-	                      ups: items[i].data.ups,
-	                      author: items[i].data.author,
-	                      domain: items[i].data.domain,
-	                      thumbnail: thumb,
-	                      commentCount: items[i].data.num_comments
-	                    }
-
-	                    $scope.posts.push(newItem);
-
-
-	                }
-	              }
-
-	            }
-
-	            $scope.nextPage = data.data.after;
-	          }
-          	} 
-	    });
-	};
-
-	  $scope.getComments = function (postId) {
-	  	$scope.comments = [];
-		JobManager.getComments($scope.subreddit, postId).then(function(data) {
-			console.log('subreddit = ' + $scope.subreddit + ' postId = ' + postId);
-			console.log(data);
-
-	        if (data.length === 0) {
-	        	$scope.error = true;
-	        	$scope.message = 'No comments found.';	
-	        	console.log('error - ' + $scope.message);
-	        } else {	
-	        console.log(data[1].data.children);	
-	          var items = data[1].data.children;
-	          console.log('items = ' + items);
-	          for (var i = 0; i < items.length; i++) {
-			    var comment = items[i].data.body;
-			    var author = items[i].data.author;
-			    //var postcomment = '<p>[Author]' + author + '<br>' + comment + '</p>';
-			    var postcomment = {
-			    	author: author,
-			    	comment: comment
-			    }
-			    $scope.comments.push(postcomment);         	
-			  }
-
-				/*$.getJSON("http://www.reddit.com/r/" + sub + "/comments/" + id + ".json?", function (data){
-				  $.each(data[1].data.children, function (i, item) {
-				    var comment = item.data.body
-				    var author = item.data.author
-				    var postcomment = '<p>[Author]' + author + '<br>' + comment + '</p>'
-				    results.append(postcomment)
-				  });
-				});*/
-			}
-
-			console.log('comments = ' + $scope.comments);	
-		});
-
-		
-
-	  }
-
-  $scope.getComments2 = function(id) {
-     $http.get("https://www.reddit.com/r/" + $scope.subreddit + "/comments/" + id + ".json").then(function (response) {
-           $scope.treeData = response.data;
-           $scope.toggleComments();
-           //$scope.checked2 = !$scope.checked2;
-      });    
-  }	  
-
+	$scope.getComments2 = function(id) {
+	   $http.get("https://www.reddit.com/r/" + $scope.subreddit + "/comments/" + id + ".json").then(function (response) {
+	   	console.log('response data = ' + response.data[1].data.children);
+		    if (response.data[1].data.children.length === 0) {
+		    	$scope.error = true;
+		    	$scope.message = 'No comments';	
+		    	console.log('error');
+		    	$scope.treeData = [];
+		    	$scope.checked2 = !$scope.checked2;
+		    } else {	  
+		    	 $scope.error = false; 	
+		         $scope.treeData = response.data;
+		         $scope.checked2 = !$scope.checked2;
+		    }
+	    }); 
+	}	     
 
   })
-
-.service('JobManager', ['$q', '$http', function($q, $http) {
-    return {
-        getAll: function (subreddit) {
-            var deferred = $q.defer();
-
-
-		    var url = "https://api.reddit.com/r/" + subreddit + "/.json?limit=5&jsonp=JSON_CALLBACK";
-
-		    $http.jsonp(url).success(function(data) {
-
-                deferred.resolve(data);
-
-            })
-            .error(function(data, status, headers, config) {
-		        deferred.resolve('');
-		    });
-
-            return deferred.promise;
-        },
-        getNext: function (subreddit, next) {
-            var deferred = $q.defer();
-			var url = "https://api.reddit.com/r/" + subreddit + "/.json?&after=" + next + "&jsonp=JSON_CALLBACK";
-        	//http://www.reddit.com/r/gaming/top.json?limit=5&after=t3_12vgdt
-
-		    $http.jsonp(url).success(function(data) {
-
-                deferred.resolve(data);
-
-            })
-            .error(function(data, status, headers, config) {
-		        deferred.resolve('');
-		    });
-
-            return deferred.promise;        	
-
-        },
-        getComments: function (subreddit, postId) {
-            var deferred = $q.defer();
-            var url = "https://api.reddit.com/r/" + subreddit + "/comments/" + postId + ".json?jsonp=JSON_CALLBACK";
-            //var url = "http://api.reddit.com/r/aww/comments/37ujsr/.json&jsonp=JSON_CALLBACK";
-			//var url = "http://api.reddit.com/r/" + subreddit + "/.json?&after=" + next + "&jsonp=JSON_CALLBACK";
-        	//var url="http://www.reddit.com/r/gaming/top.json?limit=5";
-
-		    $http.jsonp(url).success(function(data) {
-		    	console.log('success');
-                deferred.resolve(data);
-
-            })
-            .error(function(data, status, headers, config) {
-            	console.log('error ' + status);
-		        deferred.resolve('');
-		    });
-
-            return deferred.promise;             	
-        }
-    };
-}])
 
 // Reddit constructor function to encapsulate HTTP and pagination logic
 .factory('Reddit', function($http) {
@@ -305,6 +88,9 @@ console.log('postId = ' + postId + ' subreddit = ' + $scope.subreddit);
     this.items = [];
     this.busy = false;
     this.after = '';
+
+    this.treedata = [];
+    this.commentsBusy = false;        
   };
 
   Reddit.prototype.nextPage = function(subreddit) {
@@ -313,7 +99,6 @@ console.log('postId = ' + postId + ' subreddit = ' + $scope.subreddit);
     this.busy = true;
 
     var url = "https://api.reddit.com/r/" + subreddit + "?after=" + this.after + "&jsonp=JSON_CALLBACK";
-  	//console.log('Reddit = ' + url);
     $http.jsonp(url).success(function(data) {
 
 	    if (data.length === 0) {
@@ -361,7 +146,6 @@ console.log('postId = ' + postId + ' subreddit = ' + $scope.subreddit);
 
 	                 this.items.push(newItem);
 
-
 	            }
 	          }
 
@@ -371,6 +155,28 @@ console.log('postId = ' + postId + ' subreddit = ' + $scope.subreddit);
 	  	}
       this.after = "t3_" + this.items[this.items.length - 1].id;
       this.busy = false;
+    }.bind(this));
+  };
+
+  Reddit.prototype.getComments = function(subreddit, postId) {
+    if (this.commentsBusy) return;
+    this.commentsBusy = true;
+
+    //var url = "http://api.reddit.com/r/" + subreddit + "/comments/" + postId + ".json?jsonp=JSON_CALLBACK";
+    ////var url = "https://www.reddit.com/r/" + subreddit + "/comments/" + postId + ".json"
+    //$http.jsonp(url).success(function(data) {  
+    $http.get("https://www.reddit.com/r/" + subreddit + "/comments/" + postId + ".json").then(function (response) {	
+
+	    if (response.data[1].data.children.length === 0) {
+	    	$scope.error = true;
+	    	$scope.message = 'No comments found';	
+	    	console.log('error');
+	    } else {
+
+		  this.treeData = response.data;
+
+		}
+      this.commentsBusy = false;
     }.bind(this));
   };
 
